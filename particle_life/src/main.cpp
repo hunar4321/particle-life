660d78915cc7b82bde406578b8ad8f04d7112e0d
Laurent Laborde <kerdezixe@gmail.com>
2022-12-19 17:29:23 +0100

    removing intel stuff

--- particle_life/src/main.cpp
+++ particle_life/src/main.cpp
@@ -5,7 +5,7 @@
 //========================================================================
 int main( ){
 	ofGLWindowSettings s;
-	//s.setGLVersion(4, 3);	// OpenGL 4.6 ( GTX 600 and up )
+	//s.setGLVersion(4, 6);	// OpenGL 4.6 ( GTX 600 and up )
 	s.setSize(1600, 900);
 	ofCreateWindow(s);
 	ofRunApp(new ofApp());
